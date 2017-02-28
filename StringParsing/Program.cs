03d54985d4b689efa4d4066a820284b085bc7944
BaranovMykola <mapsg32@gmail.com>
2017-02-28 23:46:49 +0200

    Implemented parsing for e-mail format and removing numbers from string

---
+++ StringParsing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace StringParsing
+{
+    class Program
+    {
+        static void removeNumbers(ref string str)
+        {
+            str = Regex.Replace(str, @"\d+", "");
+            Console.WriteLine(str);
+        }
+        static bool isEmail(string mail)
+        {
+            return Regex.Match(mail, @"\w+@\w+\.\w+").Length == mail.Length;
+        }
+        static void Main(string[] args)
+        {
+            string str = "mapsg32@gmail.com";
+            Console.WriteLine("{0} is email format: {1}", str, isEmail(str));
+            Console.ReadKey();
+        }
+    }
+}
